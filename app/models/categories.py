16917b10630a974a16468df13ffc40976aad208f
Artyom Vlasov <t0mi.v@yandex.ru>
2026-06-29 16:18:19 +0300

    Update relations in category model

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 
 from app.database import Base
 
@@ -10,15 +11,14 @@ class Category(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
+    parent_id: Mapped[int | None] = mapped_column(ForeignKey("categories.id"), nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
     products: Mapped[list["Product"]] = relationship("Product", back_populates="category")
-    
-    # Обновление модели Category с самоссылающейся связью (родительская и дочерняя категории)
+
+
     parent: Mapped[Optional["Category"]] = relationship("Category",
-            remote_side="Category.id",
-            back_populates="children",
-            )
+                                                        back_populates="children",
+                                                        remote_side=[id])
     children: Mapped[list["Category"]] = relationship("Category",
-            back_populates="parent",
-            )
+                                                      back_populates="parent")
