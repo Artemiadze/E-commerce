a459a8fc4884974bdafc8d39dfcfb8c57cc8a853
Artyom Vlasov <t0mi.v@yandex.ru>
2026-07-06 10:29:50 +0300

    feat(models): create review table

--- app/models/products.py
+++ app/models/products.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey
+from sqlalchemy import String, Boolean, Integer, Numeric, ForeignKey, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -16,7 +16,9 @@ class Product(Base):
     stock: Mapped[int] = mapped_column(Integer, nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=False)
-    seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)  # New
+    seller_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, default=0.0)
 
     category: Mapped["Category"] = relationship("Category", back_populates="products")
-    seller = relationship("User", back_populates="products")
+    seller: Mapped["User"] = relationship("User", back_populates="products")
+    reviews: Mapped[list["Review"]] = relationship("Review", back_populates="product")
---
+++ app/models/reviews.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from sqlalchemy import String, Integer, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database import Base
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
+    comment: Mapped[str | None] = mapped_column(String(500), nullable=True)
+    comment_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, nullable=False)
+    grade: Mapped[int] = mapped_column(Integer, ge=1, le=5, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    product: Mapped["Product"] = relationship("Product", back_populates="reviews")
+    user: Mapped["User"] = relationship("User", back_populates="reviews")
