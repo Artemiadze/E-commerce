ed58f3f8a4ff8a94d2d9c5eca18220dd74f0de2d
Artyom Vlasov <t0mi.v@yandex.ru>
2026-09-16 09:22:02 +0300

    Update product schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .category import Category, CategoryCreate
-from .product import Product, ProductCreate
+from .product import Product, ProductCreate, ProductList
 from .user import User, UserCreate
 from .review import Review, ReviewCreate
 
-__all__ = ["Category", "CategoryCreate", "Product", "ProductCreate", "User", "UserCreate", "Review", "ReviewCreate"]
+__all__ = ["Category", "CategoryCreate", "Product", "ProductCreate", "ProductList", "User", "UserCreate", "Review", "ReviewCreate"]
